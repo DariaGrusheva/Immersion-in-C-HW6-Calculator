d518fe4a03132e64d7219bd417886fcb8e3faf48
Daria <gdaria1609@yandex.ru>
2023-12-13 14:36:21 +0500

    Добавьте файлы проекта.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Calculator : ICalc
+    {
+        public event EventHandler<EventArgs> GotResult;
+        public int Result = 0;
+        private Stack<int> Results = new Stack<int>();
+
+        public int Divide(int value)
+        {
+            Results.Push(Result);
+            Result /= value;
+            RaisEvent();
+            return Result;
+        }
+
+        public int Multiply(int value)
+        {
+            Results.Push(Result);
+            Result *= value;
+            RaisEvent();
+            return Result;
+        }
+
+        public int Subatruct(int value)
+        {
+            Results.Push(Result);
+            Result -= value;
+            RaisEvent();
+            return Result;
+        }
+
+        public int Sum(int value)
+        {
+            Results.Push(Result);
+            Result += value;
+            RaisEvent();
+            return Result;
+        }
+
+        private void RaisEvent()
+        {
+            GotResult?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void CancelLast()
+        {
+            if (Results.Count > 0)
+            {
+                Result = Results.Pop();
+                RaisEvent();
+            }
+
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Task1
+{
+    internal interface ICalc
+    {
+        event EventHandler<EventArgs> GotResult;
+        int Sum(int Value);
+        int Subatruct(int Value);
+        int Multiply(int Value);
+        int Divide(int Value);
+        void CancelLast();
+    }
+
+
+    internal class Program
+    {
+        static void Calculator_GotResult(object sendler, EventArgs evenArgs)
+        {
+            Console.WriteLine($"{((Calculator)sendler).Result}");
+        }
+        static void Main(string[] args)
+        {
+
+            ICalc calc = new Calculator();
+            calc.GotResult += Calculator_GotResult;
+
+            while (true)
+            {
+                Console.WriteLine("Нажмите: Enter - если хотите продолжить, Esc для завершения программы ");
+
+                ConsoleKeyInfo btnRead = Console.ReadKey();
+                Console.WriteLine("Введите действие или нажмите для завершения \"q\".");
+                if (btnRead.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                string? userEntered = Console.ReadLine();
+
+                if (userEntered == "q") break;
+                
+                    string? newUserEntered = userEntered.Remove(0, 1);
+                
+
+                if (int.TryParse(newUserEntered, out int value))
+                {
+                    switch (userEntered[0])
+                    {
+                        case '+':
+                            calc.Sum(value);
+                            break;
+                        case '-':
+                            calc.Subatruct(value);
+                            break;
+                        case '/':
+                            calc.Divide(value);
+                            break;
+                        case '*':
+                            calc.Multiply(value);
+                            break;
+                        default:
+                            Console.WriteLine("Вы ввели не дейсвие!");
+                            break;
+                    }
+                }
+                else { Console.WriteLine("Введите знак действия с числом!"); }
+            }
+        }
+    }
+}
